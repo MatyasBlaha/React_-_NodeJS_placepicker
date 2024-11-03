2c015ed45a46ea877f184253f197ea1ee973f6d9
MatyasBlaha <matyasblaha1@gmail.com>
2024-11-03 14:23:33 +0100

    add & remove places, handle errors

--- src/http.js
+++ src/http.js
@@ -10,3 +10,33 @@ export async function fetchAvailablePlaces(){
 
     return resData.places;
 }
+
+export async function fetchUserPlaces(){
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    if(!response.ok){
+        throw new Error('failed to fetch places')
+    }
+
+    return resData.places;
+}
+
+
+export async function updateUserPlaces(places){
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if(!response.ok){
+        throw new Error('failed to update user places')
+    }
+
+    return resData.message;
+}
